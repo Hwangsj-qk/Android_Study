b206149af94895880c4063d248fe173f4732fafb
Hwangsj-qk <sujeong7191@naver.com>
2024-06-07 14:21:45 +0900

    로그아웃 기능 구현

--- ch13_login/src/main/java/com/busanit/ch13_login/activity/MainActivity.kt
+++ ch13_login/src/main/java/com/busanit/ch13_login/activity/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.busanit.ch13_login.activity
 
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.busanit.ch13_login.R
 import com.busanit.ch13_login.RetrofitClient
 import com.busanit.ch13_login.databinding.ActivityMainBinding
 import com.busanit.ch13_login.model.Test
@@ -22,10 +23,11 @@ class MainActivity : AppCompatActivity() {
         val sharedPreferences =
             getSharedPreferences("app_pref", Context.MODE_PRIVATE)
 
+
         // 로그인 시 저장된 사용자 이름을 가져옴
         val username = sharedPreferences.getString("username", "")
 
-        binding.textViewHello.text = "안녕하세요, ${username}"
+        binding.textViewHello.text = "안녕하세요, ${username}님"
 
         // 저장된 토큰을 보호된 리소스 요청 시 사용
         val token = sharedPreferences.getString("token", "")?:""
@@ -33,10 +35,27 @@ class MainActivity : AppCompatActivity() {
         // test()      // 리팩토링
 
         // 인증 요청시 HTTP 헤더에 "Bearer {jwt_token}" 요청
-        callProtect("Bearer $token")
+        // callProtect("Bearer $token")
+
+        // 로그아웃 버튼 클릭 -> 로그아웃
+        binding.buttonLogout.setOnClickListener { logout() }
 
 
+    }
 
+    // 로그아웃 함수
+    private fun logout() {
+        // 1. SharedPreferences 에서 토큰, 사용자 정보 삭제
+        val sharedPreferences =
+            getSharedPreferences("app_pref", Context.MODE_PRIVATE)
+        sharedPreferences.edit()
+            .remove("token")
+            .remove("username")
+            .apply()
+
+        // 2, TitleActivity 로 돌려 보내고 메인 화면 종료
+        startActivity(Intent(this@MainActivity, TitleActivity::class.java))
+        finish()
     }
 
     // 보호된 자원 네트워크 요청 함수 : 403번 (금지된 응답, Forbidden, 자원 확인 불가 -> 회원가입과 로그인 과정 필요)
--- ch13_login/src/main/java/com/busanit/ch13_login/activity/TitleActivity.kt
+++ ch13_login/src/main/java/com/busanit/ch13_login/activity/TitleActivity.kt
@@ -1,5 +1,6 @@
 package com.busanit.ch13_login.activity
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -16,15 +17,30 @@ class TitleActivity : AppCompatActivity() {
         binding = ActivityTitleBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 로그인 버튼을 클릭하면 LoginActivity로 이동
-        binding.buttonLogin.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+        val token = getToken()
+
+        // 토큰이 없는 경우 타이들 화면 진행
+        if(token.isNullOrEmpty()) {
+            // 로그인 버튼을 클릭하면 LoginActivity로 이동
+            binding.buttonLogin.setOnClickListener {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+
+            // 회원가입 버튼을 클릭하면 RegisterActivity로 이동
+            binding.buttonRegister.setOnClickListener {
+                startActivity(Intent(this, RegisterActivity::class.java))
+            }
+        } else {
+            // 토큰이 존재하면 MainActivity로 이동 -> 토큰이 저장되어 있으면 재로그인할 필요가 없음(로그인 정보가 저장되어 있는 경우)
+            startActivity(Intent(this@TitleActivity, MainActivity::class.java))
+            finish()    // TitleActivity 종료
         }
 
-        // 회원가입 버튼을 클릭하면 RegisterActivity로 이동
-        binding.buttonRegister.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
-        }
+    }
 
+    // 토큰을 가져오는 메서드
+    private fun getToken() :String {
+        val sharedPreferences = getSharedPreferences("app_pref", MODE_PRIVATE)
+        return sharedPreferences.getString("token", null) ?: ""
     }
 }
